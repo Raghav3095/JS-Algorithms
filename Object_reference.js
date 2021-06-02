cc7c546de07df4c67c87bc8614dd79ff1ae11745
Raghav Awasty <raghav.awasty@gmail.com>
2021-06-02 17:53:57 +0530

    removed some comments from the file

--- Object_reference.js
+++ Object_reference.js
@@ -2,13 +2,11 @@
 
 function whatIsInAName(collection, source) {
     var arr = [];
-    // Only change code below this line
     collection.filter(function(obj) {
         for (let i = 0; i < source.length; i++) {
   
         }
     });
   
-    // Only change code above this line
     return arr;
   }
